420f38e2d5a35aab579d1fe10c16d099a30dc94c
f̣ộx̣ệ6 <foxe6@protonmail.com>
2021-05-13 06:42:50 +0800

    Add files via upload

--- shop/js/min/account.js
+++ shop/js/min/account.js
@@ -1,1 +1,1 @@
-var _location=$("meta[name='description']").attr("content");window.account_js=function(a){delete window.account_js;a.ac_info().appendTo("div#ac td#ac_info_target")};
+var _location=$("meta[name='description']").attr("content");window.account_js=function(a){delete window.account_js;a.ac_info().appendTo("div#ac td#ac_info_target");var b=$("div#ac");a.setup_scroll_hints(b);b.trigger("scroll")};
