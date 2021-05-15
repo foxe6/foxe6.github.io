8862da6221a766ad58d63aeec9fd1fb59998d351
f̣ộx̣ệ6 <foxe6@protonmail.com>
2021-05-15 22:15:19 +0800

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,7 +1,4 @@
 var pathname = window.location.pathname.replace("index.html", "");
-if (pathname[pathname.length-1] === "/") {
-    pathname = pathname.substr(0, pathname.length-1);
-}
 var pathname_index = pathname.indexOf("/",1);
 var server_name = pathname.substr(1, pathname_index-1);
 if (server_name === "root") {
@@ -11,4 +8,7 @@ else {
     server_name += ".";
 }
 pathname = pathname.substr(pathname_index);
+if (pathname[pathname.length-1] === "/") {
+    pathname = pathname.substr(0, pathname.length-1);
+}
 window.location.href = "http://"+(server_name)+"foxe6.kozow.com"+pathname+window.location.search;
