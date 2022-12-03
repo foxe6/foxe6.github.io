2c2712e5abb5933498430a6440ab2275dab25261
f̣ộx̣ệ6 <foxe6@protonmail.com>
2022-12-03 17:31:36 +0800

    Add files via upload

--- JS/jquery.ex.js
+++ JS/jquery.ex.js
@@ -196,12 +196,12 @@ $.js_download_text = function(text, name){
     a.click();
     document.body.removeChild(a);
 }
-$.new_gallery_page = function(title, imgs, dirs, fs, cb){
+$.new_gallery_page = function(title, imgs, dirs, fs, cb, local){
     window.location.hash = "";
     $("link[href='style.css']").remove();
     $.get("/404.css", function(css){
         $.get("/404.js", function(js){
-            $.get("/require=gallery_template.html", function(r){
+            $.get(local?"/require=gallery_template.html":"/?require=gallery_template", function(r){
                 r = r.replace("<imgs>", JSON.stringify(imgs));
                 r = r.replace("<dirs>", JSON.stringify(dirs));
                 r = r.replace("<fs>", JSON.stringify(fs));
